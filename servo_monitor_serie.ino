99a737ecbc32b626db779714520bd797de060890
Manuleite58 <119862332+Manuleite58@users.noreply.github.com>
2023-03-24 13:15:08 +0100

    Update and rename codigo .ino to servo_monitor_serie.ino

--- servo_monitor_serie.ino
+++ servo_monitor_serie.ino
@@ -1,5 +1,8 @@
 // C++ code
-//
+/*Este programa controla o movemento sun servo
+que esta conectado no pin 8 do arduino cun potenciometro
+conectado a entrada analoxica A0 e a vez controlado tamen 
+co monitor serie, introduciondo una valor entre 0 e 180.*/
 
 #include <Servo.h>
 
@@ -19,6 +22,7 @@ void setup() {
   Serial.begin(9600);
   //mensaje para escribir un valor
   Serial.println("Escribe un valor entre 0 y 180 para controlar el servo:");
+  //etapa de espera para poder leer a mensaxe
   delay(1000);
 }
 //comprobamos si hay orden no monitor serie
@@ -33,6 +37,8 @@ void loop() {
       delay(1500);
     }
   }
+  //funcionamiento potenciometro
+
   else {
     potPos = analogRead(pot);
     servoPos = map(potPos, 0, 1023, 0, 180);
